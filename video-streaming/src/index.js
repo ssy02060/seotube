3f304812ca2b59847a747ff0b6b56cf18c1a99f8
ssy02060 <ssy02060@gmail.com>
2022-07-23 15:21:50 -0700

    스트리밍 서비스 간소화

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,52 +1,13 @@
 const express = require("express");
 const fs = require("fs");
-const amqp = require('amqplib');
-
-if (!process.env.RABBIT) {
-    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
-}
-
-const RABBIT = process.env.RABBIT;
-
-//
-// Connect to the RabbitMQ server.
-//
-function connectRabbit() {
-
-    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
-
-    return amqp.connect(RABBIT) // Connect to the RabbitMQ server.
-        .then(connection => {
-            console.log("Connected to RabbitMQ.");
-
-            return connection.createChannel() // Create a RabbitMQ messaging channel.
-                .then(messageChannel => {
-                    return messageChannel.assertExchange("viewed", "fanout") // Assert that we have a "viewed" exchange.
-                        .then(() => {
-                            return messageChannel;
-                        });
-                });
-        });
-}
-
-//
-// Send the "viewed" to the history microservice.
-//
-function sendViewedMessage(messageChannel, videoPath) {
-    console.log(`Publishing message on "viewed" exchange.`);
-
-    const msg = { videoPath: videoPath };
-    const jsonMsg = JSON.stringify(msg);
-    messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publish message to the "viewed" exchange.
-}
 
 //
 // Setup event handlers.
 //
-function setupHandlers(app, messageChannel) {
+function setupHandlers(app) {
     app.get("/video", (req, res) => { // Route for streaming video.
 
-        const videoPath = "./videos/ddomi-chapchap.mp4";
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
         fs.stat(videoPath, (err, stats) => {
             if (err) {
                 console.error("An error occurred ");
@@ -60,8 +21,6 @@ function setupHandlers(app, messageChannel) {
             });
 
             fs.createReadStream(videoPath).pipe(res);
-
-            sendViewedMessage(messageChannel, videoPath); // Send message to "history" microservice that this video has been "viewed".
         });
     });
 }
@@ -69,14 +28,14 @@ function setupHandlers(app, messageChannel) {
 //
 // Start the HTTP server.
 //
-function startHttpServer(messageChannel) {
-    return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
+function startHttpServer() {
+    return new Promise((resolve, reject) => { // Wrap in a promise so we can be notified when the server has started.
         const app = express();
-        setupHandlers(app, messageChannel);
+        setupHandlers(app);
 
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
         app.listen(port, () => {
-            resolve(); // HTTP server is listening, resolve the promise.
+            resolve();
         });
     });
 }
@@ -85,10 +44,7 @@ function startHttpServer(messageChannel) {
 // Application entry point.
 //
 function main() {
-    return connectRabbit()                          // Connect to RabbitMQ...
-        .then(messageChannel => {                   // then...
-            return startHttpServer(messageChannel); // start the HTTP server.
-        });
+    return startHttpServer();
 }
 
 main()
