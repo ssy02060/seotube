cc651c2f79e1f041e9f1928631d34fa0c40dfe90
ssy02060 <ssy02060@gmail.com>
2022-07-23 14:40:27 -0700

    스테이트 코드 임시 닫음

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -1,11 +1,11 @@
-# Sets the "backend" used to store Terraform state.
-# This is required to make continous delivery work.
+# # Sets the "backend" used to store Terraform state.
+# # This is required to make continous delivery work.
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "seotube"
-    storage_account_name = "seotubeterraform"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "seotube"
+#     storage_account_name = "seotubeterraform"
+#     container_name       = "terraform-state"
+#     key                  = "terraform.tfstate"
+#   }
+# }
