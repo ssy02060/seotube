ff3d36be9255973d562afe88d27f2d3b5a9c3aa2
ssy02060 <ssy02060@gmail.com>
2022-07-23 14:45:46 -0700

    state test 01

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -10,26 +10,10 @@
 #   }
 # }
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
   backend "azurerm" {
     resource_group_name  = "tfstate"
     storage_account_name = "seotubeterraform"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
-
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
-  location = "eastus"
 }
