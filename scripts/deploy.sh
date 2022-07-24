df6a2d22cff6d79d46ff1f5f15a8510df353448a
ssy02060 <ssy02060@gmail.com>
2022-07-24 06:58:06 -0700

    스토리지 어카운트 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,8 @@ set -u # or set -o nounset
 : "$ARM_CLIENT_SECRET"
 : "$ARM_TENANT_ID"
 : "$ARM_SUBSCRIPTION_ID"
+: "$STORAGE_ACCOUNT_NAME"
+: "$STORAGE_ACCESS_KEY"
 
 cd ./scripts
 export KUBERNETES_SERVICE_HOST="" # Workaround for https://github.com/terraform-providers/terraform-provider-kubernetes/issues/679
@@ -15,4 +17,6 @@ terraform init
 terraform apply -auto-approve \
     -var "app_version=$VERSION" \
     -var "client_id=$ARM_CLIENT_ID" \
-    -var "client_secret=$ARM_CLIENT_SECRET"
+    -var "client_secret=$ARM_CLIENT_SECRET" \
+    -var "storage_account_name=$STORAGE_ACCOUNT_NAME" \
+    -var "storage_access_key=$STORAGE_ACCESS_KEY"
